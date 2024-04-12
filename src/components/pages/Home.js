496de8ae5ff7ea50d66fc9a7a884084e79f615a1
noussairox <abellouch34@gmail.com>
2024-04-12 18:21:27 +0000

    added sectionto HomePage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import homeImage from '../../assets/images/optimization.jpg';
-
+import FAQ from '../part/Faq';
+import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="w-full">
@@ -10,13 +11,15 @@ function Home() {
           <div className="flex flex-col justify-center items-center lg:items-start">
             <h1 className="text-4xl font-mono text-[#6C0345] mb-4">Bienvenue sur OptiDecision</h1>
             <p className="text-sm text-gray-800 mb-6 mr-5">OptiDecision est votre solution d'analyse multicritère et d'optimisation, conçue pour répondre à vos besoins professionnels les plus exigeants. Grâce à nos puissants outils basés sur les méthodes AHP et TOPSIS, vous pouvez prendre des décisions éclairées et efficaces dans une variété de domaines et de secteurs d'activité.</p>
-            <button className="bg-[#6C0345] text-white py-2 px-6 rounded-lg shadow-md hover:bg-[#4A0229] transition duration-300">Commencer</button>
+            <Link to="/demo" className="bg-[#6C0345] text-white py-2 px-6 rounded-lg shadow-md hover:bg-[#4A0229] transition duration-300 mt-2 no-underline">En savoir plus</Link>
           </div>
           {/* Section droite (Image) */}
           <div className="py-2 w-[80%]">
             <img src={homeImage} className="w-full h-auto rounded-lg shadow-md" alt="OptiDecision" />
           </div>
         </div>
+
+        <FAQ />
       </div>
     </div>
   );
