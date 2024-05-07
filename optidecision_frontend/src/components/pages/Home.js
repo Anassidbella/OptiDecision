5e6b2f7e32b612cc8170aa19837dd0bf7f74f1e0
Salma IDMANSOUR <idmansour.sa@gmail.com>
2024-05-07 22:52:38 +0100

    modification 1 de la page HOME

--- optidecision_frontend/src/components/pages/Home.js
+++ optidecision_frontend/src/components/pages/Home.js
@@ -1,27 +1,59 @@
-import React from 'react';
-import homeImage from '../../assets/images/optimization.jpg';
-import FAQ from '../part/Faq';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
+const HomeCss = {
+  container: 'max-w-none py-8 px-4 lg:py-10 lg:px-6 xl:px-12',
+  content: 'container mx-auto flex justify-between items-start',
+  title: 'text-4xl font-semibold text-[#0F1035] mb-4',
+  learnMoreButton: 'bg-[#0F1035] text-white py-2 px-6 rounded-lg shadow-md hover:bg-[#7FC7D9] transition duration-300 mt-2 no-underline',
+  cardContainer: 'flex overflow-x-auto space-x-4',
+  card: 'bg-white rounded-lg shadow-lg p-6 w-[300px] flex-shrink-0',
+  cardTitle: 'text-xl font-semibold text-[#0F1035] mb-4',
+  cardDescription: 'text-gray-800',
+};
+
 function Home() {
+  const cardContainerRef = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Défilement automatique des cartes
+      if (cardContainerRef.current) {
+        cardContainerRef.current.scrollLeft += cardContainerRef.current.offsetWidth;
+      }
+    }, 5000); // Changer la carte toutes les 5 secondes
+
+    return () => {
+      clearInterval(intervalId); // Nettoyer l'intervalle lors du démontage du composant
+    };
+  }, []);
+
   return (
-    <div className="w-full">
-      <div className="max-w-none py-8 px-4 lg:py-10 lg:px-6 xl:px-12">
-        <div className="container mx-auto flex flex-col-reverse lg:flex-row justify-between items-start">
-          {/* Section gauche */}
-          <div className="flex flex-col justify-center items-center lg:items-start">
-            <h1 className="text-4xl font-mono text-[#6C0345] mb-4">Bienvenue sur OptiDecision</h1>
-            <p className="text-sm text-gray-800 mb-6 mr-5">OptiDecision est votre solution d'analyse multicritère et d'optimisation, conçue pour répondre à vos besoins professionnels les plus exigeants. Grâce à nos puissants outils basés sur les méthodes AHP et TOPSIS, vous pouvez prendre des décisions éclairées et efficaces dans une variété de domaines et de secteurs d'activité.</p>
-            <Link to="/demo" className="bg-[#6C0345] text-white py-2 px-6 rounded-lg shadow-md hover:bg-[#4A0229] transition duration-300 mt-2 no-underline">En savoir plus</Link>
-          </div>
-          {/* Section droite (Image) */}
-          <div className="py-2 w-[80%]">
-            <img src={homeImage} className="w-full h-auto rounded-lg shadow-md" alt="OptiDecision" />
+      <div className="w-full bg-[#FDFDFD]">
+        <div className={HomeCss.container}>
+          <div className={HomeCss.content}>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className={HomeCss.title}>Bienvenue sur OptiDecision</h1>
+              <Link to="/demo" className={HomeCss.learnMoreButton}>En savoir plus</Link>
+            </div>
+            <div className={HomeCss.cardContainer} ref={cardContainerRef}>
+              <div className={HomeCss.card}>
+                <h2 className={HomeCss.cardTitle}>Analyse multicritère</h2>
+                <p className={HomeCss.cardDescription}>Utilisez nos outils d'analyse multicritère pour évaluer et comparer différentes options selon plusieurs critères et prendre des décisions éclairées.</p>
+              </div>
+              <div className={HomeCss.card}>
+                <h2 className={HomeCss.cardTitle}>Optimisation</h2>
+                <p className={HomeCss.cardDescription}>Optimisez vos processus et vos ressources grâce à nos méthodes d'optimisation avancées, basées sur les meilleures pratiques et les dernières recherches.</p>
+              </div>
+              <div className={HomeCss.card}>
+                <h2 className={HomeCss.cardTitle}>Facile à utiliser</h2>
+                <p className={HomeCss.cardDescription}>Notre plateforme est conçue pour être intuitive et conviviale, vous permettant de tirer le meilleur parti de nos fonctionnalités sans nécessiter une expertise technique approfondie.</p>
+              </div>
+              {/* Ajoutez plus de cartes ici */}
+            </div>
           </div>
         </div>
-
-        <FAQ />
       </div>
-    </div>
   );
 }
 
