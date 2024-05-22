a17dbf95fd85966e45ec6be3ee956d5ca83b9f2a
noussairox <abellouch34@gmail.com>
2024-05-22 20:53:01 +0100

    some update

--- optiDecisionBackend/optiDecisionBackend/settings.py
+++ optiDecisionBackend/optiDecisionBackend/settings.py
@@ -87,8 +87,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'SIGNING_KEY': SECRET_KEY,
